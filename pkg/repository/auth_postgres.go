7001c2743ef03f04af968ece8ac0a006607db4c5
AlibekDalgat <johnbuck_kebarty@mail.ru>
2023-01-30 00:11:46 +0300

    Authofication and identification

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -23,3 +23,10 @@ func (p *AuthPostgres) CreateUser(user todo.User) (int, error) {
 	}
 	return id, nil
 }
+
+func (p *AuthPostgres) GetUser(username, password string) (todo.User, error) {
+	var user todo.User
+	query := fmt.Sprintf("SELECT id FROM %s WHERE username=$1 AND password_hash=$2", usersTable)
+	err := p.db.Get(&user, query, username, password)
+	return user, err
+}
